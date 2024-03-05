453c3a05549b7a15f8678faf2c86834fa820a918
root <root@marufhosen>
2024-03-05 10:17:37 +0600

    fix: redis connection error & env

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,18 +1,20 @@
-import Redis from "ioredis";
+import Redis, { RedisOptions } from "ioredis";
 
 export async function connectToRedis() {
-  const redis = new Redis({
+  const redisOptions: RedisOptions = {
     host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-  });
+    port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : undefined,
+  };
+
+  const redis = new Redis(redisOptions);
 
   redis.on("error", (error: any) => {
-    console.error(`❌ Failed to connect to Redis server: ${error.message}`);
+    console.error(`Failed to connect to Redis server: ${error.message}`);
   });
 
   redis.on("connect", () => {
-    console.log("✅ Redis Connected Successfully");
+    console.log("Redis Connected Successfully");
   });
 
-  return redis; // Optionally, you can return the Redis instance
+  return redis;
 }
